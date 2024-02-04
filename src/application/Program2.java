18e1a688ea01afbd3c4e8ab1bf6fee8c1ee73f48
Desktop Marllon <marllonrsantos@gmail.com>
2024-02-04 18:01:08 -0300

    Feat: Insert implementation (Department)

---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Department newDepartment = new Department(7, "TI");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! new id = " + newDepartment.getId());
+		
+		sc.close();
+	}
+}
+
